520d59fb369335c8f9d4b85c77ae2c01464f07ab
lzf <1584212690@qq.com>
2020-11-03 19:06:50 +0800

    JS逆向解析

--- JS逆向解析有道翻译.py
+++ JS逆向解析有道翻译.py
@@ -3,6 +3,10 @@ import time
 import random
 import hashlib
 
+# 1. 确定要爬取链接 : http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule
+# 2 .请求方式  :  post
+# 3 . 构建 data 参数
+
 # 获取参数
 def get_parm(word):
     # 时间戳   1604392098660
@@ -21,19 +25,20 @@ def get_parm(word):
 
 def get_conent(data):
     url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
+    # url = 'http://fanyi.youdao.com/translate?smartresult=dict&smartresult=rule'
     headers ={
-        # 'Accept': 'application/json, text/javascript, */*; q=0.01',
-        # 'Accept-Encoding': 'gzip, deflate',
-        # 'Accept-Language': 'zh-CN,zh;q=0.9',
-        # 'Connection': 'keep-alive',
-        # 'Content-Length': '244',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Connection': 'keep-alive',
+        'Content-Length': '244',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Cookie': 'OUTFOX_SEARCH_USER_ID=-532941148@10.169.0.82; JSESSIONID=aaaRm2oJxjMzF8HaSbnwx; OUTFOX_SEARCH_USER_ID_NCOO=1595231732.1175306; ___rl__test__cookies=1604391991434',
-        # 'Host': 'fanyi.youdao.com',
-        # 'Origin': 'http://fanyi.youdao.com',
+        'Host': 'fanyi.youdao.com',
+        'Origin': 'http://fanyi.youdao.com',
         'Referer': 'http://fanyi.youdao.com/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
-        # 'X-Requested-With': 'XMLHttpRequest',
+        'X-Requested-With': 'XMLHttpRequest',
     }
     data = {
         'i':data['word'],    # 需要翻译单词
